9fe5376c413bfa715bf826ef465c48f36fcc5884
alexBigBoss <armando.sepulvedacrz@gmail.com>
2015-10-07 23:19:02 -0500

    Pantallas de Pago
    Vista de las pantallas de Pago

--- Appetite/app/src/main/java/com/appetite/appetite/model/Noticias.java
+++ Appetite/app/src/main/java/com/appetite/appetite/model/Noticias.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-
 import com.appetite.appetite.R;
 
 public class Noticias extends Activity implements OnClickListener {
